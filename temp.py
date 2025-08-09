9ee31319a946d76e1a6db549a6edfcaada45b368
Ravi Ranjan <137782361+raviranjancom@users.noreply.github.com>
2025-08-10 03:02:06 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,11 @@
+temperature = float(input("Enter the temperature : "))
+unit=input("Enter C/F : ")
+
+if(unit=="C"):
+    temperature=(9/5*temperature)+32
+    temperature=round(temperature,2)
+    print(f"The temperature = {temperature} F")
+else:
+    temperature=5/9*(temperature-32)
+    temperature=round(temperature,2)
+    print(f"The temperature = {temperature} C");
